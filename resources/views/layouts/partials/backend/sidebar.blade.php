3abb2e43cb4fc5a2668c6763fed3e6a65b06df6a
creativehabib <shamolrahaman@gmail.com>
2025-03-16 15:30:30 +0600

    update

--- resources/views/layouts/partials/backend/sidebar.blade.php
+++ resources/views/layouts/partials/backend/sidebar.blade.php
@@ -56,7 +56,7 @@
                 </li>
                 <li class="slide has-sub">
                     <a href="javascript:void(0);" class="side-menu__item">
-                        <i class="ti ti-dice-4  side-menu__icon"></i>
+                        <i class="ti ti-checkup-list side-menu__icon"></i>
                         <span class="side-menu__label">Pages</span>
                         <i class="fe fe-chevron-right side-menu__angle"></i>
                     </a>
